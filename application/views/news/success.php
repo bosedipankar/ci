66457c00268504fa7111d6a9e852f58c5d12819c
Dipankar Bose <bose_slg@yahoo.com>
2017-02-02 21:18:22 +0530

    project changes

---
+++ application/views/news/success.php
@@ -0,0 +1,1 @@
+<h2>News Article has been added to the Database </h2>
---
+++ application/views/templates/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+  <body>
+         <h2><?php echo $title;  ?></h2>
